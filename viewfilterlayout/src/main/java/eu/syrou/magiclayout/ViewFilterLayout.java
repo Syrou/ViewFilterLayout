e77bb305a67c400fc3584147a61ecf164c3347f9
Syrou <syroumusic@gmail.com>
2016-09-19 19:49:56 +0200

    third commit

--- viewfilterlayout/src/main/java/eu/syrou/magiclayout/ViewFilterLayout.java
+++ viewfilterlayout/src/main/java/eu/syrou/magiclayout/ViewFilterLayout.java
@@ -13,42 +13,42 @@ import android.widget.RelativeLayout;
 /**
  * Created by Syrou on 2016-09-18.
  */
-public class MagicLayout extends RelativeLayout {
+public class ViewFilterLayout extends RelativeLayout {
 
-    private static final String TAG = MagicLayout.class.getSimpleName();
+    private static final String TAG = ViewFilterLayout.class.getSimpleName();
 
     SparseArray<View> mMagicLayoutChilden = new SparseArray<>();
     int priorityIterator = 1;
 
 
-    public MagicLayout(Context context) {
+    public ViewFilterLayout(Context context) {
         super(context);
     }
 
-    public MagicLayout(Context context, AttributeSet attrs) {
+    public ViewFilterLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
         initAttributes(context, attrs);
     }
 
-    public MagicLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+    public ViewFilterLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         initAttributes(context, attrs);
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public MagicLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+    public ViewFilterLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         initAttributes(context, attrs);
     }
 
     @Override
     public RelativeLayout.LayoutParams generateLayoutParams(AttributeSet attrs) {
-        return new MagicLayout.LayoutParams(getContext(), attrs);
+        return new ViewFilterLayout.LayoutParams(getContext(), attrs);
     }
 
     @Override
     protected boolean checkLayoutParams(ViewGroup.LayoutParams p) {
-        return p instanceof MagicLayout.LayoutParams;
+        return p instanceof ViewFilterLayout.LayoutParams;
     }
 
     @Override
@@ -58,7 +58,7 @@ public class MagicLayout extends RelativeLayout {
 
     private void initAttributes(Context context, AttributeSet attributeSet) {
 
-        TypedArray arr = context.obtainStyledAttributes(attributeSet, R.styleable.MagicLayout);
+        TypedArray arr = context.obtainStyledAttributes(attributeSet, R.styleable.ViewFilterLayout);
         //int priority = arr.getInteger(R.styleable.MagicLayout_hidePriority, 0);
 
         // do something here with your custom property
@@ -85,8 +85,8 @@ public class MagicLayout extends RelativeLayout {
                 totalChildHeight = totalChildHeight + vh;
             }
 
-            if (child.getLayoutParams() instanceof MagicLayout.LayoutParams) {
-                MagicLayout.LayoutParams layoutParams = (LayoutParams) child.getLayoutParams();
+            if (child.getLayoutParams() instanceof ViewFilterLayout.LayoutParams) {
+                ViewFilterLayout.LayoutParams layoutParams = (LayoutParams) child.getLayoutParams();
                 if (layoutParams.hidePriority != -1 && child.getVisibility() != GONE) {
                     mMagicLayoutChilden.put(layoutParams.hidePriority, child);
                 }
@@ -128,8 +128,8 @@ public class MagicLayout extends RelativeLayout {
 
         public LayoutParams(Context context, AttributeSet attrs) {
             super(context, attrs);
-            TypedArray arr = context.obtainStyledAttributes(attrs, R.styleable.MagicLayout_LayoutParams);
-            hidePriority = arr.getInteger(R.styleable.MagicLayout_LayoutParams_hidePriority, -1);
+            TypedArray arr = context.obtainStyledAttributes(attrs, R.styleable.ViewFilterLayout_LayoutParams);
+            hidePriority = arr.getInteger(R.styleable.ViewFilterLayout_LayoutParams_filterPriority, -1);
             arr.recycle();
         }
 
